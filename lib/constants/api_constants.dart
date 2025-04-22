93bc85dec7daa3304d49ace618a0ffc1214a177b
Julio <gogczoro@gmail.com>
2025-04-22 12:38:51 -0600

    add api's endpoints as constants

---
+++ lib/constants/api_constants.dart
@@ -0,0 +1,5 @@
+class ApiConstants {
+  static const String baseURL = 'https://api.mironline.io/api/v1/';
+  static String userLoginURL = '${baseURL}alumno/login';
+  static String userFetchURL = '${baseURL}alumno';
+}
