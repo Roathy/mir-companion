fd0b74aa4ee28c29a9b9396824821ec72629f7c6
Julio <gogczoro@gmail.com>
2025-04-23 12:00:47 -0600

    move template files

---
+++ lib/lab_features/06_a11_unit1_activities/view/pages/a11_unit1_activities_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/icon_header.dart';
+import '../widgets/scrollable_vertical_list.dart';
+
+class A11Activities extends StatelessWidget {
+  const A11Activities({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+            extendBodyBehindAppBar: true,
+            extendBody: true,
+            appBar: AppBar(backgroundColor: Colors.transparent, automaticallyImplyLeading: false, actions: [
+              Column(children: [
+                IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: const Icon(
+                      Icons.arrow_back,
+                      color: Colors.white,
+                    )),
+              ]),
+            ]),
+            body: const Stack(children: [
+              ScrollableVerticalList(),
+              IconHeader(
+                level: 'A1.1',
+                subtitle: 'Choose a unit:',
+              ),
+            ])));
+  }
+}
