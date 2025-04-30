54924f005e78ab4421c26fcc664c0f2d0d3ff3ed
Julio <gogczoro@gmail.com>
2025-04-30 16:58:50 -0600

    implement secure storage starting logic

--- lib/core/infra/storage/secure_storage.dart
+++ lib/core/infra/storage/secure_storage.dart
@@ -12,9 +12,22 @@ class SecureStorageServiceImpl implements SecureStorageService {
   SecureStorageServiceImpl(this._storage);
 
   @override
-  Future<String?> read(String key) {}
+  Future<String?> read(String key) async {
+    try {
+      return await _storage.read(key: key);
+    } catch (e) {
+      // Handle any errors that may occur during read operation
+      return null;
+    }
+  }
+
   @override
-  Future<void> write(String key, String value) {}
+  Future<void> write(String key, String value) async {
+    await _storage.write(key: key, value: value);
+  }
+
   @override
-  Future<void> delete(String key) {}
+  Future<void> delete(String key) async {
+    await _storage.delete(key: key);
+  }
 }
