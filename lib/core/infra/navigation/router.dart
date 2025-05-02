0a816776cdc5c581a10a0ca106767f6580d5e8fc
Julio <gogczoro@gmail.com>
2025-05-02 16:08:09 -0600

    delete route

--- lib/core/infra/navigation/router.dart
+++ lib/core/infra/navigation/router.dart
@@ -1,5 +1,7 @@
 import 'package:go_router/go_router.dart';
 
+import '../../../features/00_splash_refactor/presentation/pages/splash_screen.dart';
+
 class AppRoutes {
   static const String splash = '/splash';
   static const String login = '/login';
@@ -9,9 +11,6 @@ class AppRoutes {
 final GoRouter appRouter = GoRouter(initialLocation: AppRoutes.splash, routes: [
   GoRoute(
     path: AppRoutes.splash,
-    // builder: (context, state) => SplashScreen(),
-  ),
-  GoRoute(
-    path: AppRoutes.login,
+    builder: (context, state) => const SplashScreen(),
   ),
 ]);
