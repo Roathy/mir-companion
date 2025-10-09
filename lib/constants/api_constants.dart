7089fb9c5eeaa82efd1dfe25cfe442e14c50f1f3
Julio <gogczoro@gmail.com>
2025-10-09 09:33:56 -0600

    fix endpoint constants

--- lib/constants/api_constants.dart
+++ lib/constants/api_constants.dart
@@ -1,6 +1,6 @@
 class ApiConstants {
-  static const String baseURL = 'https://api.mironline.io/api/v1/';
-  static String userLoginURL = '${baseURL}students/login';
-  static String userFetchURL = '${baseURL}students/today';
-  static String checkAlive = '${baseURL}students/check-alive';
+  static const String baseURL = 'https://api.mironline.io/api/v1/students';
+  static String userLoginURL = '$baseURL/login';
+  static String userFetchURL = '$baseURL/today';
+  static String checkAlive = '$baseURL/check-alive';
 }
