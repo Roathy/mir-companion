c5cc8bdaccfe76fc5b24d659f452484137dd438f
Julio <gogczoro@gmail.com>
2025-10-15 13:45:49 -0600

    update function to call new dependency

--- lib/core/utils/crypto.dart
+++ lib/core/utils/crypto.dart
@@ -1,13 +1,13 @@
 import 'dart:convert';
 
 import 'package:crypto/crypto.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:mironline/env/env.dart';
 
 String createMD5Hash() {
   DateTime now = DateTime.now();
   String formattedDate =
       '${now.year}${now.month.toString().padLeft(2, '0')}${now.day.toString().padLeft(2, '0')}';
   return md5
-      .convert(utf8.encode('${dotenv.env['SECRET_KEY']}-$formattedDate'))
+      .convert(utf8.encode('${Env.secretKey}-$formattedDate'))
       .toString();
 }
