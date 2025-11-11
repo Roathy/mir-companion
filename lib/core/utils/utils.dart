876afb4ba0812641a59972d66468b27f88f24c16
Julio <gogczoro@gmail.com>
2025-11-11 11:12:47 -0600

    remove unsued and obsolete code from the project

--- lib/core/utils/utils.dart
+++ lib/core/utils/utils.dart
@@ -1,5 +1,3 @@
 export 'crypto.dart';
 export 'extract_path.dart';
 export 'format_buy_msg.dart';
-export 'get_auth_token.dart';
-export 'is_token_expired.dart';
--- lib/features/01_welcome_tour/data/datasource/welcome_slides.dart
+++ lib/features/01_welcome_tour/data/datasource/welcome_slides.dart
@@ -2,16 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../domain/entity/slide.dart';
-import '../../widgets/radar_chart.dart';
 
 final slidesProvider = Provider<List<Slide>>((ref) => [
       // COMPLEMENT
       Slide(
         title: 'The perfect complement for daily practice',
         textContent: Wrap(children: [
-          Text.rich(TextSpan(text: 'mironline ', style: const TextStyle(color: Colors.purple, fontSize: 18, fontWeight: FontWeight.bold), children: <InlineSpan>[
-            TextSpan(text: 'is a platform that has been developed based on contexts, situations and interactions that will allow you to  make the most of the system and technologies to practice the English language in a new way and beyond your textbook.', style: TextStyle(color: Colors.purple[400], fontSize: 18, fontWeight: FontWeight.w300))
-          ]))
+          Text.rich(TextSpan(
+              text: 'mironline ',
+              style: const TextStyle(
+                  color: Colors.purple,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold),
+              children: <InlineSpan>[
+                TextSpan(
+                    text:
+                        'is a platform that has been developed based on contexts, situations and interactions that will allow you to  make the most of the system and technologies to practice the English language in a new way and beyond your textbook.',
+                    style: TextStyle(
+                        color: Colors.purple[400],
+                        fontSize: 18,
+                        fontWeight: FontWeight.w300))
+              ]))
         ]),
         assetUrl: 'assets/animations/landing_hands_animation.riv',
       ),
@@ -19,17 +30,35 @@ final slidesProvider = Provider<List<Slide>>((ref) => [
       // FIELD OF STUDY
       Slide(
         title: 'English in your field of study*',
-        textContent: Text.rich(TextSpan(text: 'mironline', style: const TextStyle(color: Colors.purple, fontSize: 18, fontWeight: FontWeight.bold), children: <InlineSpan>[
-          TextSpan(text: ' offers modules to develop English for the fields of Agriculture, Environment, Construction, Engineering, Computing, Electronics, Economics, Administration, Health Sciences, Humanities and Arts. Enjoy!', style: TextStyle(color: Colors.purple[400], fontSize: 18, fontWeight: FontWeight.w300))
-        ])),
+        textContent: Text.rich(TextSpan(
+            text: 'mironline',
+            style: const TextStyle(
+                color: Colors.purple,
+                fontSize: 18,
+                fontWeight: FontWeight.bold),
+            children: <InlineSpan>[
+              TextSpan(
+                  text:
+                      ' offers modules to develop English for the fields of Agriculture, Environment, Construction, Engineering, Computing, Electronics, Economics, Administration, Health Sciences, Humanities and Arts. Enjoy!',
+                  style: TextStyle(
+                      color: Colors.purple[400],
+                      fontSize: 18,
+                      fontWeight: FontWeight.w300))
+            ])),
         assetUrl: 'assets/animations/landing_stars_animation.riv',
-        note: '*ESP modules are only available for the Make it Real! Professional and Make it Real! English for Higher Education in Latin America book series',
+        note:
+            '*ESP modules are only available for the Make it Real! Professional and Make it Real! English for Higher Education in Latin America book series',
       ),
 
       // INTEGRATED ACTIVITIES
       Slide(
         title: 'Integrated Skills Activities*',
-        textContent: Text(style: TextStyle(color: Colors.purple[400], fontSize: 18, fontWeight: FontWeight.w300), 'Improve your listening, writing and speaking skills through integrated activities that will be unlocked at the end of each General English unit.'),
+        textContent: Text(
+            style: TextStyle(
+                color: Colors.purple[400],
+                fontSize: 18,
+                fontWeight: FontWeight.w300),
+            'Improve your listening, writing and speaking skills through integrated activities that will be unlocked at the end of each General English unit.'),
         note: '*ISA activities are just available for the Go high! book series',
         assetUrl: 'assets/animations/landing_isa_animation.riv',
       ),
@@ -38,22 +67,53 @@ final slidesProvider = Provider<List<Slide>>((ref) => [
       Slide(
         title: 'Practicing is fun',
         textContent: Wrap(children: [
-          Text.rich(TextSpan(text: 'General English activities are organized by levels. You must unlock each level to progress to the next one. With ', style: TextStyle(color: Colors.purple[400], fontSize: 18, fontWeight: FontWeight.w300), children: <InlineSpan>[
-            const TextSpan(text: 'mironline', style: TextStyle(color: Colors.purple, fontSize: 18, fontWeight: FontWeight.bold)),
-            TextSpan(text: ', practicing English is as fun as playing your favorite game on your phone, tablet or other device.', style: TextStyle(color: Colors.purple[400], fontSize: 18, fontWeight: FontWeight.w300))
-          ]))
+          Text.rich(TextSpan(
+              text:
+                  'General English activities are organized by levels. You must unlock each level to progress to the next one. With ',
+              style: TextStyle(
+                  color: Colors.purple[400],
+                  fontSize: 18,
+                  fontWeight: FontWeight.w300),
+              children: <InlineSpan>[
+                const TextSpan(
+                    text: 'mironline',
+                    style: TextStyle(
+                        color: Colors.purple,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold)),
+                TextSpan(
+                    text:
+                        ', practicing English is as fun as playing your favorite game on your phone, tablet or other device.',
+                    style: TextStyle(
+                        color: Colors.purple[400],
+                        fontSize: 18,
+                        fontWeight: FontWeight.w300))
+              ]))
         ]),
         assetUrl: 'assets/animations/landing_stars_animation.riv',
       ),
 
       // PROGRESS
       Slide(
-          title: 'Follow your progress',
-          textContent: Wrap(children: [
-            Text.rich(TextSpan(text: 'mironline ', style: const TextStyle(color: Colors.purple, fontSize: 18, fontWeight: FontWeight.bold), children: <InlineSpan>[
-              TextSpan(text: 'tracks your progress. You will be able to review your progress in skills, work and language activities. This will help you identify your strengths and areas for improvement.', style: TextStyle(color: Colors.purple[400], fontSize: 18, fontWeight: FontWeight.w300))
-            ]))
-          ]),
-          assetUrl: '',
-          multimedia: const RadarChart()),
+        title: 'Follow your progress',
+        textContent: Wrap(children: [
+          Text.rich(TextSpan(
+              text: 'mironline ',
+              style: const TextStyle(
+                  color: Colors.purple,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold),
+              children: <InlineSpan>[
+                TextSpan(
+                    text:
+                        'tracks your progress. You will be able to review your progress in skills, work and language activities. This will help you identify your strengths and areas for improvement.',
+                    style: TextStyle(
+                        color: Colors.purple[400],
+                        fontSize: 18,
+                        fontWeight: FontWeight.w300))
+              ]))
+        ]),
+        assetUrl: '',
+        // multimedia: const RadarChart()),
+      )
     ]);
